adf68940ada0914f0d865a85ee97d956d31fef96
kensiiwasaki <kensiiwasaki@gmail.com>
2023-02-09 23:14:31 +0900

    readonlyを用いた制限

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 
-const obj = { a: 1 }
-obj.a = 2
-console.log(obj)
+// const obj = { a: 1 }
+// obj.a = 2
+// console.log(obj)
+let obj: {
+  readonly a: number
+}
+obj = { a: 1 }
 
-const nums = [1, 2]
-nums.push(3)
-console.log(nums)
+// const nums = [1, 2]
+// nums.push(3)
+// console.log(nums)
+
+const nums: readonly number[] = [1, 2, 3]
 
 const Home: NextPage = () => {
   return <div className="text-blue-500">Hello!</div>
