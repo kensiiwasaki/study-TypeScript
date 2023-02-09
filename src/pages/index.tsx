086f07d10ca5d9ac161de8b1d8bada849f96752f
kensiiwasaki <kensiiwasaki@gmail.com>
2023-02-09 22:44:57 +0900

    オブジェクトのプロパティはミュータブルである

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from "next";
 
+const obj = { a: 1 };
+obj.a =  2 ;
+console.log(obj);
+
+
 const Home: NextPage = () => {
   return <div className="text-blue-500">Hello!</div>;
 };
