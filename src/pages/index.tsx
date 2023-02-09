ead2fcc3d2a2e6894b8a0b606369fd864b321eb8
kensiiwasaki <kensiiwasaki@gmail.com>
2023-02-09 22:47:18 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
-const obj = { a: 1 };
-obj.a =  2 ;
-console.log(obj);
-
+const obj = { a: 1 }
+obj.a = 2
+console.log(obj)
 
 const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
-};
+  return <div className="text-blue-500">Hello!</div>
+}
 
-export default Home;
+export default Home
