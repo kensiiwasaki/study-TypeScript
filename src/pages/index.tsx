570c9b25178988b5be48cce4b72a20cceb18a468
kensiiwasaki <kensiiwasaki@gmail.com>
2023-02-09 22:54:50 +0900

    配列も同様

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 
-const obj = { a: 1 }
-obj.a = 2
-console.log(obj)
+const num = [1, 2]
+num.push(3)
+console.log(num)
 
 const Home: NextPage = () => {
   return <div className="text-blue-500">Hello!</div>
