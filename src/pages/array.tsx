3d7f024aee236affe8955621fcfdd26b5f6eb0e2
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-03 06:46:23 +0900

    配列の型アノテーションは２通りある

--- src/pages/array.tsx
+++ src/pages/array.tsx
@@ -3,3 +3,11 @@ import type { NextPage } from 'next'
 // 配列リテラル
 // 配列リテラルは普通に配列を書くように記述
 ;[1, 2, 3]
+
+// 配列の型アノテーション
+// 配列の型アノテーションは２通りある
+let array1: number[]
+array1 = [1, 2, 3]
+
+let array2: Array<number>
+array2 = [1, 2, 3]
