114217b14066376f0bd92925dd17070c582c67d1
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-04 08:30:23 +0900

    タプルで戻り値の型を指定できる

--- src/pages/tuple.tsx
+++ src/pages/tuple.tsx
@@ -2,10 +2,17 @@ import type { NextPage } from 'next'
 
 // タプル
 // TSの関数でreturnできるのは１値のみなので、複数返したい時は配列にまとめる
-function tuple() {
+function tuple1() {
   //...
   return [1, 'ok', true]
 }
 // この場合tupleを呼び出す時の型がany[]かunknown[]になってしまう
 // unknown[]だとメソッドを呼び出すことができない
-const list: unknown[] = tuple()
+const list1: unknown[] = tuple1()
+// そこでタプルを使用する
+function tuple2(): [number, string, boolean] {
+  //...
+  return [1, 'ok', true]
+}
+
+const list2: [number, string, boolean] = tuple2()
