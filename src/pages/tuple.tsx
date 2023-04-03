a9c6d90bdc144dc7c9b404f8081e3e8756c9efd2
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-04 08:23:14 +0900

    配列の戻り値の問題点

--- src/pages/tuple.tsx
+++ src/pages/tuple.tsx
@@ -1,1 +1,11 @@
 import type { NextPage } from 'next'
+
+// タプル
+// TSの関数でreturnできるのは１値のみなので、複数返したい時は配列にまとめる
+function tuple() {
+  //...
+  return [1, 'ok', true]
+}
+// この場合tupleを呼び出す時の型がany[]かunknown[]になってしまう
+// unknown[]だとメソッドを呼び出すことができない
+const list: unknown[] = tuple()
