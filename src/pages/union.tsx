b13bb0557d7df0a9abb537d0281ffcf12a32e30f
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-20 22:13:54 +0900

    型の絞り込み

--- src/pages/union.tsx
+++ src/pages/union.tsx
@@ -6,3 +6,13 @@ let numberOrUndefined: number | undefined
 type List1 = string | number[]
 // 両方を配列型にする場合は下記
 type List2 = (string | number)[]
+
+// ユニオン型と絞り込み
+const maybeUserId: string | null = localStorage.getItem('userId')
+
+const userId: string = maybeUserId //この書き方だとuserIdがnullの可能性がありエラー
+
+// typeofで型を取得することできるのでそれを使って絞り込む
+if (typeof maybeUserId === 'string') {
+  const userId: string = maybeUserId // この分岐内では文字列型に絞り込まれるため、代入できる。
+}
