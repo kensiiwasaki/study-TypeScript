36e7106cb969a68fa4a864b99ea4d3098dba3b1f
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-20 22:38:44 +0900

    修正

--- src/pages/intersection.tsx
+++ src/pages/intersection.tsx
@@ -5,5 +5,13 @@ type TwoDimensionalPoint = {
   x: number
   y: number
 }
+type Z = {
+  z: number
+}
+
+type ThreeDimensionalPoint = TwoDimensionalPoint & Z
 // そのため下記はエラーになる
-const twoDimensionalPoint: TwoDimensionalPoint = { x: 1 }
+const p: ThreeDimensionalPoint = {
+  x: 0,
+  y: 1,
+}
