a4d45dab991d6079ab7448486b86c90d79fa4bc4
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-20 23:19:07 +0900

    間違った型アサーションはしっかりエラーを出してくれる

--- src/pages/typeAssertion.tsx
+++ src/pages/typeAssertion.tsx
@@ -3,3 +3,7 @@ import type { NextPage } from 'next'
 // 型推論より正しい値を意図的に上書きできる
 const value: string | number = 'this is a string'
 const strLength: number = (value as string).length
+
+// 間違った型アサーションはしっかりエラーを出してくれる
+const num = 123
+const str: string = num as string
