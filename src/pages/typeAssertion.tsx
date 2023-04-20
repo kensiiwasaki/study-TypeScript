c3357eef05b2e2f34b935fa0a8a83606d7a63de1
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-20 23:18:19 +0900

    コメント修正

--- src/pages/typeAssertion.tsx
+++ src/pages/typeAssertion.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
 
-// 型推論より正しい値を意図的に伝えることができる
+// 型推論より正しい値を意図的に上書きできる
 const value: string | number = 'this is a string'
 const strLength: number = (value as string).length
