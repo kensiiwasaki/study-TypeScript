9074a6026602bc0f9d110afa60573482e7b7537f
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-18 21:56:41 +0900

    pending

--- src/pages/Promise.tsx
+++ src/pages/Promise.tsx
@@ -225,3 +225,5 @@ Promise.race([request1(), request2(), request3()]).then((num) => {
 // Promise深掘り
 // Promiseの状態
 // Promiseには３つの状態がある -> pending、fulfilled、rejected
+
+// pending -> 待機中という意味で、待ち受けている非同期処理が完了していない時の状態を示す
