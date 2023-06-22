6d73a08291462786f204704218c360760b5b6243
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-22 23:22:19 +0900

    未定義の変数fooへの代入

--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from 'next'
 
 // スクリプトとモジュール
 // スクリプト
-const foo = 'foo'
+const foo1 = 'foo'
 
 // モジュール
-export const foo = 'foo'
+export const foo2 = 'foo'
 
 // 値の公開非公開
 // exportつけた値だけが公開され、他のモジュールから参照できる
@@ -16,3 +16,6 @@ export const publicValue = 1
 const privateValue = 2
 
 // モジュールは常にstrict-mode
+// 未定義の変数fooへの代入は不可
+foo = 1
+export const bar = foo
