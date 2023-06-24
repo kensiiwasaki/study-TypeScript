ca90b116501365398ad8b328ca7cd89ccbb816d8
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-24 23:49:54 +0900

    上と下は同じ意味になる

--- src/pages/Partial.tsx
+++ src/pages/Partial.tsx
@@ -1,2 +1,16 @@
 // Partial<T>
 // <T>をすべてオプショナルにする
+
+// 例
+type Person = {
+  surname: string
+  middleName?: string
+  givenName: string
+}
+type PartialPerson = Partial<Person>
+// 上と下は同じ意味になる
+type PartialPerson = {
+  surname?: string
+  middleName?: string
+  givenName?: string
+}
