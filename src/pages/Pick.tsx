3fde6d63552542765e2de63c5596821fb8afaef9
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-24 23:58:56 +0900

    例

--- src/pages/Pick.tsx
+++ src/pages/Pick.tsx
@@ -1,2 +1,15 @@
 // Pick<T, Keys>
 // <T>から指定したキーを持つオブジェクトの型を返す
+
+// 例
+type User = {
+  surname: string
+  middleName?: string
+  givenName: string
+  age: number
+  address?: string
+  nationality: string
+  createdAt: string
+  updatedAt: string
+}
+type Person = Pick<User, 'surname' | 'middleName' | 'givenName'>
