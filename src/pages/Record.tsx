076a5db7a556a95bf16364a80bf23c81d3b26400
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-24 23:56:36 +0900

    キーがいづれかになるオブジェクトの型も定義できる

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -4,3 +4,10 @@
 // 例
 type StringNumber = Record<string, number>
 const value: StringNumber = { a: 1, b: 2, c: 3 }
+// キーがいづれかになるオブジェクトの型も定義できる
+type Person = Record<'firstName' | 'middleName' | 'lastName', string>
+const person: Person = {
+  firstName: 'Robert',
+  middleName: 'Cecil',
+  lastName: 'Martin',
+}
