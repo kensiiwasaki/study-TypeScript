023eb7f0c4887b395e19ec6a91b24f58220499d3
kensiiwasaki <kensiiwasaki@gmail.com>
2023-07-07 23:48:39 +0900

    例

--- src/pages/Omit.ts
+++ src/pages/Omit.ts
@@ -1,2 +1,22 @@
 // Omit<T, Keys>
 // Omit<T, Keys>はkeysで指定したプロパティを除いたobjectを返すユーティリティ型
+
+// 例
+type User = {
+  surname: string
+  middleName?: string
+  givenName: string
+  age: number
+  address?: string
+  nationality: string
+  createdAt: string
+  updatedAt: string
+}
+type Optional = 'age' | 'address' | 'nationality' | 'createdAt' | 'updatedAt'
+type Person = Omit<User, Optional>
+// 上と下は同じ意味になる
+type Person = {
+  surname: string
+  middleName?: string
+  givenName: string
+}
