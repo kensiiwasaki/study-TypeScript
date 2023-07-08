f9c1e0769242514afbf2c8232a1f9be9951b4d37
kensiiwasaki <kensiiwasaki@gmail.com>
2023-07-08 23:08:31 +0900

    キー制約として使用することができる

--- src/pages/MappedTypes.ts
+++ src/pages/MappedTypes.ts
@@ -1,3 +1,10 @@
 // Mapped Types
 // インデックス型はどのようなキーも自由に設定できてしまい、アクセス時に毎回undefinedのチェックが必要
 // 入力形式が決まっている場合はMappedTypeの使用を検討できる
+
+// 主にユニオン型と組み合わせて使う
+type SystemSupportLanguage = 'en' | 'fr' | 'it' | 'es'
+// これをインデックス型と同じようにキー制約として使用することができる
+type Butterfly = {
+  [key in SystemSupportLanguage]: string
+}
